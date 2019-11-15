52df64ec3721359847f95a6fde66db9c9885561b
Laureano Sanchez <lagasa95@hotmail.com>
2019-11-15 16:22:14 -0600

    Close HIS-2

--- src/app/crud/application_data_operations.py
+++ src/app/crud/application_data_operations.py
@@ -10,7 +10,7 @@ def get_application_by_id(db: Session, application_id: uuid.UUID):
 
 
 # ---------------------------------------------------------------------------------------
-# GET EMPLOYEES
+# GET APPLICATIONS
 # ---------------------------------------------------------------------------------------
 def get_applications(db: Session, skip: int = 0, limit: int = 100, **kwargs):
     query = db.query(models.IsxApplication)
@@ -22,3 +22,44 @@ def get_applications(db: Session, skip: int = 0, limit: int = 100, **kwargs):
             models.IsxApplication.is_enabled == enabled
         )
     return query.offset(skip).limit(limit).all()
+
+
+# -----------------------------------------------------------------------------
+# CREATE APPLICATION
+# -----------------------------------------------------------------------------
+def create_application(db: Session, role: schemas.Application):
+    db_application = models.IsxApplication(**role.dict())
+    db_application.application_id = uuid.uuid4()
+    db.add(db_application)
+    db.commit()
+    db.refresh(db_application)
+    return db_application
+
+
+# -----------------------------------------------------------------------------
+# UPDATE APPLICATION
+# -----------------------------------------------------------------------------
+def update_application(application_id: str, db: Session, role: schemas.Application):
+    role_dict: dict = role.dict()
+    role = db.query(models.IsxApplication)\
+        .filter(models.IsxApplication.id == uuid.uuid4(application_id))\
+        .first()
+    if role:
+        for key in role_dict.keys():
+            if role_dict.get(key):
+                setattr(role, key, role_dict.get(key))
+        db.commit()
+        db.refresh(role)
+    return role
+
+
+# -----------------------------------------------------------------------------
+# SOFT DELETE APPLICATION
+# -----------------------------------------------------------------------------
+def soft_delete_application(application_id: str, db: Session, role: schemas.Application):
+    role = db.query(models.IsxApplication).filter(models.IsxApplication.id == uuid.uuid4(application_id)).first()
+    if role:
+        setattr(role, "is_soft_deleted", True)
+        db.commit()
+        db.refresh(role)
+    return role
--- src/app/routers/application.py
+++ src/app/routers/application.py
@@ -8,7 +8,7 @@ import uuid
 router = APIRouter()
 
 # ---------------------------------------------------------------------------------------
-# GET SAMPLE
+# GET IDENTITY
 # ---------------------------------------------------------------------------------------
 @router.get("/application", tags=["v1"], response_model=List[schemas.Application])
 async def list_application_definitions(is_enabled: str = None, skip: int = 0, limit: int = 100,
